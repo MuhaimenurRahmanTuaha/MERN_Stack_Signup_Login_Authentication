cd1d7508f3a82f92670586daea57d47ad642654b
Muhaimenur Rahman Tuaha <muhaimenurrahmantuaha@gmail.com>
2025-11-13 15:49:03 +0800

    index vercel change

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,3 @@
-require("dotenv").config();
-const app = require("./app");
-
-PORT = process.env.PORT || 8081;
-
-app.listen(PORT,()=>{
-    console.log(`server is running at http://localhost:${PORT}`);
-})
+// api/index.js
+const app = require("../app");
+module.exports = app;
