e24436bb28acf5a79861c42491057f0b2f0e701d
MuhaimenurRahmanTuaha <155227737+MuhaimenurRahmanTuaha@users.noreply.github.com>
2025-11-13 17:31:45 +0800

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
 
   const fetchProducts = async () => {
     try {
-      const url = "http://localhost:8080/products";
+      const url = "mern-stack-signup-login-authenticat.vercel.app";
       const headers = {
         headers: {
           'Authorization': localStorage.getItem('token')
